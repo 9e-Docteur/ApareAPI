e55d5e10eaa8db5798f1dd5ca24ce44963284ead
Loris P <lorispopulaire@gmail.com>
2024-03-07 22:44:08 +0100

    Updated Logger (commit for updated SC too)

--- src/main/java/be/ninedocteur/apare/utils/SharedConstants.java
+++ src/main/java/be/ninedocteur/apare/utils/SharedConstants.java
@@ -2,6 +2,6 @@ package be.ninedocteur.apare.utils;
 
 public class SharedConstants {
     public static String NAME = "ApareAPI";
-    public static String VERSION = "1.4.6";
+    public static String VERSION = "1.4.7";
     public static String STATE = "release";
 }
